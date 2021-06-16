5cfcd80bc2edf32f5b3edf95b3636321887a8ac6
hieuhieu01 <85987389+hieuhieu01@users.noreply.github.com>
2021-06-16 15:28:11 +0700

    kkkk

---
+++ Set04/Set04_TrinhChinChin/build/generated-sources/ap-source-output/aptech/entity/Employee_.java
@@ -0,0 +1,17 @@
+package aptech.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-18T23:47:31")
+@StaticMetamodel(Employee.class)
+public class Employee_ { 
+
+    public static volatile SingularAttribute<Employee, String> employeeName;
+    public static volatile SingularAttribute<Employee, String> phone;
+    public static volatile SingularAttribute<Employee, String> employeeID;
+    public static volatile SingularAttribute<Employee, String> job;
+    public static volatile SingularAttribute<Employee, Double> salary;
+
+}
